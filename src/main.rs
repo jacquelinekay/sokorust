d9ead5a3d40a3c21b6036b7f51fadb814302acd7
Vittorio Romeo <vittorio.romeo@outlook.com>
2017-05-15 15:50:56 +0100

    Playing around with strong typedefs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,56 @@
 /*
  * */
-use std::io;
-use std::mem;
 
+use std::io;
 mod handle_vector;
 
-const width: usize = 10;
-const height: usize = 10;
+#[derive (Copy, Clone)]
+struct XCoord(usize);
+
+#[derive (Copy, Clone)]
+struct YCoord(usize);
+
+#[derive (Copy, Clone)]
+struct Position(XCoord, YCoord);
+
+#[derive (Copy, Clone)]
+struct XDiff(i32);
+
+#[derive (Copy, Clone)]
+struct YDiff(i32);
+
+#[derive (Copy, Clone)]
+struct Offset(XDiff, YDiff);
+
+impl XCoord {
+    fn offset(&self, x: XDiff) -> Self {
+        XCoord(((self.0 as i32) + x.0) as usize)
+    }
+}
+
+impl YCoord {
+    fn offset(&self, y: YDiff) -> Self {
+        YCoord(((self.0 as i32) + y.0) as usize)
+    }
+}
+
+impl Position {
+    fn offset(mut self, o: Offset) -> Self {
+        self.0 = self.0.offset(o.0);
+        self.1 = self.1.offset(o.1);
+        self
+    }
+}
+
+
+
+const WIDTH: usize = 10;
+const HEIGHT: usize = 10;
+
+
+
+
+
 
 #[derive (Copy, Clone)]
 enum Entity {
@@ -17,32 +61,24 @@ enum Entity {
 }
 
 struct State {
-    board: [Entity; width * height],
-    player_pos: (usize, usize),
-}
-
-fn apply_offset(x: usize, offset: i32) -> usize {
-    ((x as i32) + offset) as usize // TODO: idiomatic?
-}
-
-fn apply_offset_to_vector((x, y): (usize, usize), (ox, oy): (i32, i32)) -> (usize, usize) {
-    (apply_offset(x, ox), apply_offset(y, oy))
+    board: [Entity; WIDTH * HEIGHT],
+    player_pos: Position,
 }
 
 impl State {
     fn new() -> Self {
         State {
-            board: [Entity::Floor; width * height], // TODO: idiomatic? (repeating size)
-            player_pos: (0, 0),
+            board: [Entity::Floor; WIDTH * HEIGHT], // TODO: idiomatic? (repeating size)
+            player_pos: Position(XCoord(0), YCoord(0)),
         }
     }
 
     fn print(&self) {
         // x = i % 10
         // y = i / 10
-        for y in 0..height {
-            for x in 0..width {
-                let entity = &self.board[x + y * width];
+        for y in 0..HEIGHT {
+            for x in 0..WIDTH {
+                let entity = &self.board[x + y * WIDTH];
                 match entity {
                     &Entity::Player => print!("@"),
                     &Entity::Wall => print!("#"),
@@ -56,10 +92,10 @@ impl State {
 
     fn load(x: &str) -> Option<State> {
         let mut state = State::new();
-        for i in 0..(width * height) {
+        for i in 0..(WIDTH * HEIGHT) {
             state.board[i] = match x.chars().nth(i).unwrap() {
                 '@' => {
-                    state.player_pos = (i % width, i / height);
+                    state.player_pos = Position(XCoord(i % WIDTH), YCoord(i / HEIGHT));
                     Entity::Player
                 }
                 '#' => Entity::Wall,
@@ -71,16 +107,16 @@ impl State {
         return Option::Some(state);
     }
 
-    fn swap_tile(&mut self, x0: (usize, usize), x1: (usize, usize)) {
+    fn swap_tile(&mut self, x0: Position, x1: Position) {
         self.board.swap(Self::to_index(x0), Self::to_index(x1));
     }
 
-    fn tile_at(&self, pos: (usize, usize)) -> &Entity {
+    fn tile_at(&self, pos: Position) -> &Entity {
         &self.board[Self::to_index(pos)]
     }
 
-    fn move_thing(&mut self, pos: (usize, usize), offset: (i32, i32)) -> bool {
-        let next_pos = apply_offset_to_vector(pos, offset);
+    fn move_thing(&mut self, pos: Position, offset: Offset) -> bool {
+        let next_pos = pos.offset(offset);
         match self.tile_at(next_pos) {
             &Entity::Floor => {
                 let old_pos = pos;
@@ -92,9 +128,9 @@ impl State {
         }
     }
 
-    fn move_player(&mut self, offset: (i32, i32)) {
+    fn move_player(&mut self, offset: Offset) {
         let old_pos = self.player_pos;
-        let next_pos = apply_offset_to_vector(old_pos, offset);
+        let next_pos = old_pos.offset(offset);
 
         match *self.tile_at(next_pos) {
             Entity::Floor => {
@@ -103,7 +139,7 @@ impl State {
             }
             Entity::Crate => {
                 let success = self.move_thing(next_pos, offset);
-                if (success) {
+                if success {
                     self.move_player(offset);
                 }
             }
@@ -112,9 +148,9 @@ impl State {
         }
     }
 
-    fn to_index(p: (usize, usize)) -> usize {
+    fn to_index(p: Position) -> usize {
         match p {
-            (x, y) => (x + y * width) as usize,
+            Position(XCoord(x), YCoord(y)) => (x + y * WIDTH) as usize,
         }
     }
 }
@@ -132,17 +168,16 @@ fn main() {
                                  ##########")
             .unwrap();
 
-            return;
     loop {
         print!("{}[2J", 27 as char);
         state.print();
         let mut input = String::new();
         io::stdin().read_line(&mut input);
         match input.as_str() {
-            "w\n" => state.move_player((0, -1)),
-            "a\n" => state.move_player((-1, 0)),
-            "s\n" => state.move_player((0, 1)),
-            "d\n" => state.move_player((1, 0)),
+            "w\n" => state.move_player(Offset(XDiff(0), YDiff(-1))),
+            "a\n" => state.move_player(Offset(XDiff(-1), YDiff(0))),
+            "s\n" => state.move_player(Offset(XDiff(0), YDiff(1))),
+            "d\n" => state.move_player(Offset(XDiff(1), YDiff(0))),
             _ => (),
         };
     }
